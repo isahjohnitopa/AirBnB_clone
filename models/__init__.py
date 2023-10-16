a0ca3c49865216b290e8a8ead545f626847ce50b
isahjohnitopa <ijohnabdulmalik@gmail.com>
2023-10-16 13:19:15 -0700

    fixing error

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
+import os
 """create a unique FileStorage instance for your application"""
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
